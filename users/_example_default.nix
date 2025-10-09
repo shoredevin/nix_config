098e7dd508fb9462366a948d9ca7749c81cfd417
shoredevin <shore.devin@gmail.com>
2025-10-09 16:37:13 -0400

    adding configurable users

---
+++ users/_example_default.nix
@@ -0,0 +1,10 @@
+# this is an example user import file,
+# either copy this file to ./defailt.nix as is 
+# or remove/comment unwanted users
+
+{
+  imports = [
+    ./dshore.nix
+    ./senna.nix
+  ];
+}
