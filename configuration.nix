0598f2730cfd94a93660e692952bb6162e33488d
Devin Shore <shore.devin@gmail.com>
2025-10-10 15:34:47 -0400

    switching to grub

--- configuration.nix
+++ configuration.nix
@@ -15,13 +15,14 @@
     ];
 
   # Bootloader.
-  # boot.loader.systemd-boot.enable = false;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.device = "nodev";
-  # boot.loader.grub.useOSProber = true;
-  
-  boot.loader.systemd-boot.enable = true;  
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";  
+ 
+  # boot.loader.systemd-boot.enable = true;  
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
--- init.sh
+++ init.sh
@@ -6,4 +6,8 @@ if [ ! -f "./users/default.nix" ]; then
   	cp "./users/_example_default.nix" "./users/default.nix"
 fi
 sudo ln -s "$dir/configuration.nix" /etc/nixos/configuration.nix
+
+nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
+nix-channel --update
+
 sudo nixos-rebuild switch
