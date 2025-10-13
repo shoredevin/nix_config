92cff65f1be97f2910f880aa3660a5b0b9a49779
= <=>
2025-10-13 09:41:02 -0400

    backgrounds and vscode configs

--- init.sh
+++ init.sh
@@ -1,11 +1,20 @@
 dir=$(pwd)
+
 if [ -f "/etc/nixos/configuration.nix" ]; then
 	sudo rm /etc/nixos/configuration.nix
 fi
+
+if [ -d "/etc/nixos/users" ]; then
+	sudo rm -r "/etc/nixos/users"
+fi
+
 if [ ! -f "./users/default.nix" ]; then
   	cp "./users/_example_default.nix" "./users/default.nix"
 fi
+
 sudo ln -s "$dir/configuration.nix" /etc/nixos/configuration.nix
+sudo ln -s "$dir/users" /etc/nixos
+
 
 nix-channel --add https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz home-manager
 nix-channel --update
