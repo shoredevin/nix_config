8a4bcdfc5d1a028e715b3067bf14fd7763ff04a5
Devin Shore <shore.devin@gmail.com>
2025-10-30 20:59:17 -0400

    cleaning up init file

--- init.sh
+++ init.sh
@@ -3,22 +3,16 @@ dir=$(pwd)
 # check if hostname file exists and if not create it
 if [ ! -f "./hostname.nix" ]; then
 	   fileContents="{ config, lib, pkgs, modulesPath, ... }:
-    
+
 	   {
    		   networking.hostName = __hostname;
 	   }"
-    
 	   replaceString="__hostname"
 	   # newString="\"test\""
 	   read -p "Enter hostname: " hostname
-    
 	   newFileContents=$(echo "$fileContents" | sed "s/$replaceString/\"$hostname\"/")
-    
 	   echo "$newFileContents" > hostname.nix
-else
-    echo "file already exists"
 fi
-
 
 if [ -f "/etc/nixos/configuration.nix" ]; then
 	sudo rm /etc/nixos/configuration.nix
@@ -26,6 +20,10 @@ fi
 
 if [ -d "/etc/nixos/users" ]; then
 	sudo rm -r "/etc/nixos/users"
+fi
+
+if [ -f "/etc/nixos/hostname.nix" ]; then
+	sudo rm /etc/nixos/hostname.nix
 fi
 
 if [ ! -f "./users/default.nix" ]; then
@@ -41,4 +39,6 @@ sudo nix-channel --update
 
 sudo nixos-rebuild switch
 
-git clone https://github.com/catppuccin/cosmic-desktop.git ~/Documents/catppuccin
+if [ -d "~/Documents/catpuccin" ]; then
+	git clone https://github.com/catppuccin/cosmic-desktop.git ~/Documents/catppuccin
+fi
