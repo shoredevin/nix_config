173d363f3ee8d0fb9ae24caaf51a1352b5624f7e
= <=>
2025-11-13 12:02:36 -0500

    ignoring hostname and adding packages for melonds and others

--- configuration.nix
+++ configuration.nix
@@ -179,6 +179,7 @@
     koboldcpp
     uwsm
     chromium
+    melonDS
     # vscode
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
