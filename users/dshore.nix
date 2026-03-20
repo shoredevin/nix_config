b7f838e401f9c97212ce9ff8777abda8c2ca064d
Devin Shore <shore.devin@gmail.com>
2026-03-20 12:32:49 -0400

    switching to flakes

--- users/dshore.nix
+++ users/dshore.nix
@@ -2,7 +2,7 @@
 
 {
   imports = [
-    <home-manager/nixos>
+    # <home-manager/nixos>
   ];
 
   users.users.dshore = {
@@ -10,7 +10,7 @@
     description = "Devin Shore";
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
-	  neovim
+      neovim
       fastfetch
       atuin
       home-manager
@@ -24,63 +24,8 @@
       nicotine-plus
       nodejs
       # pgadmin4
+      wireshark
     ];
-  };
-
-home-manager.users.dshore = { pkgs, ... }: {
-    home.packages = [ pkgs.atool pkgs.httpie ];
-    home.stateVersion = "25.05";
-    home.file.".nanorc".text = "
-	set linenumbers
-        set tabsize 4
-        set titlecolor white,black
-        set numbercolor white,black
-    ";
-   home.file.".tmux.conf".text = ''
-	'';
-    programs.atuin = {
-      enable = true;
-      settings = {
-        auto_sync = true;
-	enter_accept = true;
-	records = true;
-      };
-    };
-    programs.bash = {
-      enable = true;
-      bashrcExtra = "
-	    alias nc='sudo nano /etc/nixos/configuration.nix'
-	    alias nr='sudo nixos-rebuild switch'
-	    alias ls='eza --icons'
-	    alias la='eza -a --icons'
-        alias ll='eza -a -l -B --icons'
-   	    alias devin='ll /etc/nixos'
-	    alias dude='echo hello'
-	    alias nu='~/Documents/nix_config/update.sh'
-      ";
-    };
-    programs.tmux = {
-      enable = true;
-      clock24 = true;
-	  baseIndex = 1;
-    };
-    programs.starship = {
-      enable = true;
-    };
-    nixpkgs.config.allowUnfree = true;
-    programs.vscode = {
-      enable = true;
-      package = pkgs.vscode;
-      profiles.default.extensions = with pkgs.vscode-extensions; [
-        bbenoist.nix
-        ritwickdey.liveserver
-	    catppuccin.catppuccin-vsc
-      ];
-      profiles.default.userSettings = {
-		"workbench.colorTheme" = "Catppuccin Mocha";
-        "workbench.startupEditor" = "none";
-      };
-    };
   };
 
   services.postgresql = {
@@ -88,7 +33,7 @@ home-manager.users.dshore = { pkgs, ... }: {
     ensureDatabases = [ "mydatabase" ];    
     authentication = pkgs.lib.mkOverride 10 ''
 	#type    database    DBuser    origin-address	  auth-method
-    local    all         all       			          trust
+        local    all         all       			  trust
 	host     all         all       127.0.0.1/32       trust
 	host     all         all       ::1/128            trust
     '';
