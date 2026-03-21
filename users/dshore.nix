9c23967c884065658a7ef7cdd142537825fea5f6
= <=>
2026-03-20 20:03:57 -0400

    moving files that can be deleted

--- users/dshore.nix
+++ users/dshore.nix
@@ -33,7 +33,7 @@
     ensureDatabases = [ "mydatabase" ];    
     authentication = pkgs.lib.mkOverride 10 ''
 	#type    database    DBuser    origin-address	  auth-method
-        local    all         all       			  trust
+    local    all         all       			  		  trust
 	host     all         all       127.0.0.1/32       trust
 	host     all         all       ::1/128            trust
     '';
