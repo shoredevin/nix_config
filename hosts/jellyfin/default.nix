0cc3368565ae1d69573cbeeaa3fa18e0270056a4
shore.devin <shore.devin@gmail.com>
2026-07-17 14:46:51 -0400

    adding remote desktop functionality to jellyfin

--- hosts/jellyfin/default.nix
+++ hosts/jellyfin/default.nix
@@ -2,5 +2,11 @@
 
 {
   services.jellyfin.enable = true;
-  networking.firewall.allowedTCPPorts = [ 8096 8920];
+  networking.firewall.allowedTCPPorts = [ 8096 8920 47990 ];
+  services.sunshine = {
+    enable = true;
+   	autoStart = true;
+	capSysAdmin = true;
+	openFirewall = true;
+  };
 }
