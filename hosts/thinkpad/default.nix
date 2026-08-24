fbf097dca13e6138057f8aab8ccf3b9982ba4289
Devin Shore <shore.devin@gmail.com>
2026-08-24 15:26:07 -0400

    adding battery config for thinkpad/girls laptop

---
+++ hosts/thinkpad/default.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{ 
+    boot.blacklistedKernelModules = [ "raydium_i2c_ts" ];
+
+    services.power-profiles-daemon.enable = false;
+    services.tlp = {
+        enable = true;
+        settings = {
+            CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+            CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+            WIFI_PWR_ON_BAT = "on";
+            START_CHARGE_THRESH_BAT1 = 75;            
+            STOP_CHARGE_THRESH_BAT1 = 80;
+        };
+    };
+    services.thermald.enable = true;
+    environment.systemPackages = with pkgs; [
+        btop
+    ];
+}
