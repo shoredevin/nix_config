165a81c37cdb8b1a3c77b8aa5d4baebc3cbe5f3f
Devin Shore <shore.devin@gmail.com>
2026-08-25 13:41:33 -0400

    making dev module check for user

--- modules/apps/dev.nix
+++ modules/apps/dev.nix
@@ -1,36 +1,55 @@
 { config, pkgs, lib, ... }:
 
-{
-  environment.systemPackages = with pkgs; [
-    postgresql
-    dbeaver-bin
-    nodejs
-    docker-compose
-    postman
-    androidStudioPackages.stable	 
-  ];
-  
-  virtualisation.docker.enable = true;
-  
-  services.postgresql = {
-    enable = true;
-    ensureDatabases = [ "mydatabase" ];    
-    authentication = pkgs.lib.mkOverride 10 ''
-      #type    database    DBuser    origin-address	  auth-method
-      local    all         all       			  		      trust
-      host     all         all       127.0.0.1/32     trust
-      host     all         all       ::1/128          trust
-    '';
+let
+  cfg = config.modules.dev;
+in {
+  options.modules.dev = {
+    enable = lib.mkEnableOption "Dev utils";
+
+    users = lib.mkOption {
+      type = lib.types.listOf lib.types.str;
+      default = [ ];
+      description = "Users who get dev utils";
+    };
   };
 
-  networking.firewall.allowedTCPPorts = [ 
-    3000 
-    3002
-    4000
-    5001 
-  ];
-  
-  networking.extraHosts = ''
-	127.0.0.1 tv.testinglocal.com
-  '';
+  config = lib.mkIf cfg.enable {
+    users.users = lib.genAttrs ([ "dshore" ] ++ cfg.users) (user: {
+      packages = with pkgs; [
+        postgresql
+        dbeaver-bin
+        nodejs
+        docker-compose
+        postman
+        androidStudioPackages.stable	 
+      ];
+      
+      extraGroups = [ "docker" ];
+    });
+
+    virtualisation.docker.enable = true;
+    
+    services.postgresql = {
+      enable = true;
+      ensureDatabases = [ "mydatabase" ];    
+      authentication = pkgs.lib.mkOverride 10 ''
+        #type    database    DBuser    origin-address	  auth-method
+        local    all         all       			  		      trust
+        host     all         all       127.0.0.1/32     trust
+        host     all         all       ::1/128          trust
+      '';
+    };
+
+    networking.firewall.allowedTCPPorts = [ 
+      3000 
+      3002
+      4000
+      5001 
+    ];
+    
+    networking.extraHosts = ''
+      127.0.0.1 tv.testinglocal.com
+    '';
+    
+  };
 }
