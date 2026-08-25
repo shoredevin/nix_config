0df823721bd211e33c708ac126b7e7364abfe659
Devin Shore <shore.devin@gmail.com>
2026-08-25 11:50:45 -0400

    updating common to remove atuin

--- config.sh
+++ config.sh
@@ -1,9 +1,9 @@
-
+hostname=$(hostname)
 if [ -z "$1" ]; then
- file="configuration.nix"
+ file="flake.nix"
 else
    if [ "$1" = "-c" ]; then
-     file="configuration.nix"
+     file="/modules/core/common.nix"
    fi
    if [ "$1" = "-f" ]; then
       file="flake.nix"
@@ -11,12 +11,14 @@ else
    if [ "$1" = "-u" ]; then
      file="users/dshore/default.nix"
    fi
-   # file=$1
-fi
-if [ -z "$file" ]; then
-  echo "$1 is not a valid argument. Valid arguments are: -c -f -u"
-  exit -1
+   if [ "$1" = "-h" ]; then
+	 file="hosts/$hostname/default.nix"
+   fi
 fi
 
+if [ -z "$file" ]; then
+  echo "$1 is not a valid argument. Valid arguments are: -c -f -u -h"
+  exit -1
+fi
 
 nano "~/Documents/nix_config/$file";
