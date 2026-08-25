089f7602ba30b57aa51c38f5cce5e3adf6c303ae
Devin Shore <shore.devin@gmail.com>
2026-08-25 15:42:48 -0400

    cleaning up home manager

--- flake.nix
+++ flake.nix
@@ -22,6 +22,7 @@
         home-manager.nixosModules.home-manager {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
+home-manager.backupFileExtension = "backup";
         }
       ];
       
