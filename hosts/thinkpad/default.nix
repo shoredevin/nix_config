8e08d9765fbe26681a07921634c7030a3189df6e
Devin Shore <shore.devin@gmail.com>
2026-08-26 12:30:34 -0400

    adding auto update to thinkpad

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -4,6 +4,7 @@
     imports = [
         ./hardware-configuration.nix
         ../../modules/core/desktop.nix
+        ../../modules/core/auto-update.nix
         ../../users/miya/default.nix
         ../../users/senna/default.nix
     ];
