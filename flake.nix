62f7f3f288dd0fbe9977d03d08e704245df0b5b3
Devin Shore <shore.devin@gmail.com>
2026-08-27 15:42:37 -0400

    intial setup for poo

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
         thunkpad   = mkHost "thunkpad" [ ];
         livingroom = mkHost "livingroom" [ ];
         jellyfin   = mkHost "jellyfin" [ ];
-		poopbook   = mkHost "poopbook" [ ];
+		poo   = mkHost "poo" [ ];
       };  
     };  
 }
