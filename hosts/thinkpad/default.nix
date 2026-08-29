b4865b3b2f76ff228799863dba0d42ff69f27114
Devin Shore <shore.devin@gmail.com>
2026-08-29 12:27:57 -0400

    adding more bat config for thinkpad

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -11,6 +11,9 @@
 
     boot.blacklistedKernelModules = [ "raydium_i2c_ts" ];
 
+    boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
+    boot.kernelModules = [ "acpi_call" "thinkpad_acpi" ];
+
     services.power-profiles-daemon.enable = false;
     services.tlp = {
         enable = true;
@@ -18,6 +21,8 @@
             CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
             CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
             WIFI_PWR_ON_BAT = "on";
+            START_CHARGE_THRESH_BAT0 = 75;
+            STOP_CHARGE_THRESH_BAT0 = 80;
             START_CHARGE_THRESH_BAT1 = 75;            
             STOP_CHARGE_THRESH_BAT1 = 80;
         };
