770fde49c8cae5259af42f4be7be85f991f39ca2
Devin Shore <shore.devin@gmail.com>
2026-09-02 12:48:52 -0400

    adding arm docker to jellyfin

--- hosts/jellyfin/default.nix
+++ hosts/jellyfin/default.nix
@@ -13,7 +13,7 @@
   };
 
   services.jellyfin.enable = true;
-  networking.firewall.allowedTCPPorts = [ 8096 8920 47990 ];
+  networking.firewall.allowedTCPPorts = [ 8080 8096 8920 47990 ];
   services.sunshine = {
     enable = true;
     autoStart = true;
@@ -25,5 +25,37 @@
 	hello
 	cowsay
   ];
+
+
+# Enable Docker or Podman (Docker is used in this example)
+  virtualisation.docker.enable = true;
+
+  # Declaratively manage the ARM container
+  virtualisation.oci-containers.backend = "docker";
+  virtualisation.oci-containers.containers.arm-rippers = {
+    image = "automaticrippingmachine/automatic-ripping-machine:latest";
+    autoStart = true;
+    ports = [
+      "8080:8080"
+    ];
+    environment = {
+      ARM_UID = "1000"; # Adjust to match host user ID if needed
+      ARM_GID = "1000"; # Adjust to match host group ID if needed
+    };
+    volumes = [
+      "/home/arm:/home/arm"
+      "/home/arm/Music:/home/arm/Music"
+      "/home/arm/logs:/home/arm/logs"
+      "/home/arm/media:/home/arm/media"
+      "/home/arm/config:/etc/arm/config"
+    ];
+    devices = [
+      "/dev/sr0:/dev/sr0" # Pass your optical drive mapping
+    ];
+    extraOptions = [
+      "--privileged"
+      "--restart=always"
+    ];
+  };
 
 }
