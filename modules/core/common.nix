9227e93fe91f39fcf52c30066f949e1cb496d4bb
Devin Shore <shore.devin@gmail.com>
2026-09-10 15:50:30 -0400

    adding root ssh key access

--- modules/core/common.nix
+++ modules/core/common.nix
@@ -79,6 +79,14 @@ in
     ports = [ 22 ];
   };
 
+  users.users.root = {
+    openssh.authorizedKeys.keys = [
+	  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB2gQThX+UZdmH1Vxkx9AB2zxDsfABvMwHY5ZJO8vt33 dshore@ED2025-013"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICLzinTEZyzErp3QyMRiEqOH2t8mqIE3DVQMYO8Ec2hD dshore@thunkpad"
+	  "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICp/dR4f/hd1gDTCKJ4eCUvJqbDMuzh9brvgc3kYfKlS dshore@office"
+    ];
+  };
+
   sops = {
     defaultSopsFile = ../../secrets/secrets.yaml;
     defaultSopsFormat = "yaml";
