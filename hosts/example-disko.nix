c4064029f39221992986e4f859e0c78571aae89a
Devin Shore <shore.devin@gmail.com>
2026-09-11 08:41:31 -0400

    updating bootstrap script

---
+++ hosts/example-disko.nix
@@ -0,0 +1,35 @@
+{ device ? "/dev/sda", ... }:
+
+{
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = device;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "umask=0077" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
